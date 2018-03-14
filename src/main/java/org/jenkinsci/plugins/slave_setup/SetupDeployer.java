8af3ddc2c9639f2981ead7fb103b62ec5d5862aa
tom-cn <changlong@outlook.com>
2018-03-14 17:06:11 +0800

    Update SetupDeployer.java

--- src/main/java/org/jenkinsci/plugins/slave_setup/SetupDeployer.java
+++ src/main/java/org/jenkinsci/plugins/slave_setup/SetupDeployer.java
@@ -1,111 +1,260 @@
 package org.jenkinsci.plugins.slave_setup;
 
-
-import com.google.common.base.Strings;
-import hudson.AbortException;
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Launcher;
-import hudson.model.Descriptor;
+import hudson.*;
+import hudson.model.Computer;
+import hudson.model.Label;
+import hudson.model.Node;
 import hudson.model.TaskListener;
-import hudson.slaves.ComputerLauncher;
-import hudson.slaves.DelegatingComputerLauncher;
-import hudson.slaves.SlaveComputer;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
 import hudson.tasks.Shell;
-import hudson.EnvVars;
+import hudson.util.LogTaskListener;
 import jenkins.model.Jenkins;
-import org.kohsuke.stapler.DataBoundConstructor;
+import org.apache.commons.lang.StringUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Implements the custom logic for an on-demand slave, executing scripts before connecting and after disconnecting
+ * Executes a deployment to all or a single node of the given fileset and executes the command line.
+ *
+ * @author Frederik Fromm
  */
-public class SetupSlaveLauncher extends DelegatingComputerLauncher {
+public class SetupDeployer {
+    /**
+     * the logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(SetupDeployer.class.getName());
 
-    private final String startScript;
-    private final String stopScript;
+    /**
+     * Returns a list of all active slaves connected to the master.
+     *
+     * @return a list of all active slaves connected to the master
+     */
+    public List<Computer> getAllActiveSlaves() {
+        final List<Computer> computers = Arrays.asList(Jenkins.getInstance().getComputers());
 
-    @DataBoundConstructor
-    public SetupSlaveLauncher(ComputerLauncher launcher,
-                              String startScript,
-                              String stopScript) {
-        super(launcher);
-        this.startScript = startScript;
-        this.stopScript = stopScript;
+        List<Computer> activeComputers = new ArrayList<Computer>();
+
+        for (Computer computer : computers) {
+            if (!(computer instanceof Jenkins.MasterComputer) && computer.isOnline()) {
+                activeComputers.add(computer);
+            }
+        }
+
+        return activeComputers;
     }
 
     /**
-     * Executes a script on the master node, with a bit of tracing.
+     * @param c        the computer to upload th files to
+     * @param root     the computer's target directory
+     * @param listener the listener for logging etc.
+     * @param config   the SetupConfig object containing the all SetupConfigItems
+     * @throws IOException
+     * @throws InterruptedException
      */
-    private void execute(String script, TaskListener listener, SlaveComputer computer) throws IOException, InterruptedException {
-        Jenkins jenkins = Jenkins.getInstance();
+    public void deployToComputer(Computer c, FilePath root, TaskListener listener, SetupConfig config) throws IOException, InterruptedException {
+        List<SetupConfigItem> setupConfigItems = config.getSetupConfigItems();
 
-        if (jenkins == null) {
-            listener.getLogger().println("Jenkins is not ready... doing nothing");
+        for (SetupConfigItem setupConfigItem : setupConfigItems) {
+            deployToComputer(c, root, listener, setupConfigItem);
+        }
+    }
+
+    /**
+     * @param c               the computer to upload th files to
+     * @param root            the computer's target directory
+     * @param listener        the listener for logging etc.
+     * @param setupConfigItem the SetupConfigItem object containing the source dir and the command line
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void deployToComputer(Computer c, FilePath root, TaskListener listener, SetupConfigItem setupConfigItem) throws IOException, InterruptedException {
+        // do not deploy is prepare script for this setup config item did not work
+        if (!setupConfigItem.isPrepareScriptExecuted()) {
+            listener.getLogger().println("Slave NOT set up as prepare script was not executed successfully.");
             return;
         }
 
-        if (Strings.isNullOrEmpty(script)) {
-            listener.getLogger().println("No script to be executed for this on-demand slave.");
-            return;
-        }
-
-            Launcher launcher = jenkins.getRootPath().createLauncher(listener);
-            Shell shell = new Shell(script);
-            FilePath root = jenkins.getRootPath();
-            FilePath scriptFile = shell.createScriptFile(root);
-            EnvVars envVars = new EnvVars();
-            envVars.put("SLAVE_NAME", computer.getName());
-            int r = launcher.launch().cmds(shell.buildCommandLine(scriptFile)).envs(envVars).stdout(listener).join();
-
-            if (r != 0) {
-                throw new AbortException("Script failed with return code " + Integer.toString(r) + ".");
+        //do not deploy if label of computer and config do not match.
+        if (checkLabels(c, setupConfigItem)) {
+            // copy files
+            File sd = setupConfigItem.getFilesDir();
+            if (sd != null && StringUtils.isNotBlank(sd.getPath())) {
+                listener.getLogger().println("Copying setup script files from " + sd);
+                new FilePath(sd).copyRecursiveTo(root);
             }
 
-            listener.getLogger().println("Script executed successfully. private void execute");
+            // execute command line
+            String cmdLine = setupConfigItem.getCommandLine();
 
+            executeScript(c.getNode(), root, listener, cmdLine, createEnvVarsForComputer(c));
+        } else {
+            listener.getLogger().println("Slave " + c.getName() + " NOT set up as assigned label expression '" + setupConfigItem.getAssignedLabelString() + "' does not match with node label '" + c.getNode().getLabelString() + "'");
+        }
     }
 
-    /*
-     * Getters for Jelly
+    /**
+     * Checks if this script should be executed on the target computer. If the computer is not set (on save of the
+     * jenkins configuration page) or if the label expression of the config matches with the given
+     * computer.
      */
-    public String getStartScript() {
-        return startScript;
+    private boolean checkLabelsForComputerOrNull(Computer c, SetupConfigItem item) {
+        return c == null || checkLabels(c, item);
     }
 
-    public String getStopScript() {
-        return stopScript;
-    }
-
-    /*
-     *  Delegated methods that plug the additional logic for on-demand slaves
+    /**
+     * Returns true if the given setup config item is responsible for the given slave computer.
+     *
+     * @param c               the slave computer
+     * @param setupConfigItem the setup config item to check
+     * @return true if the given setup config item is responsible for the given slave computer
      */
+    public boolean checkLabels(Computer c, SetupConfigItem setupConfigItem) {
+        String assignedLabel = setupConfigItem.getAssignedLabelString();
+        if (StringUtils.isBlank(assignedLabel)) {
+            return true;
+        }
 
-    @Override
-    public void launch(SlaveComputer computer, TaskListener listener) throws IOException, InterruptedException {
-        execute(startScript, listener, computer);
-        super.launch(computer, listener);
+        //Label l = Jenkins.getInstance().getLabel(setupConfigItem.getAssignedLabelString());
+        Label label = Label.get(assignedLabel);
+
+        return label.contains(c.getNode());
     }
 
-    @Override
-    public void afterDisconnect(SlaveComputer computer, TaskListener listener) {
-        super.afterDisconnect(computer, listener);
+    private void executeScript(Node node, FilePath root, TaskListener listener, String cmdLine, EnvVars additionalEnvironment) throws IOException, InterruptedException {
+        if (StringUtils.isNotBlank(cmdLine)) {
+            String nodeName = node.getNodeName();
+            listener.getLogger().println("Executing script '" + cmdLine + "' on " + (StringUtils.isEmpty(nodeName) ? "master" : nodeName));
+            Launcher launcher = root.createLauncher(listener);
+            Shell s = new Shell(cmdLine);
+            FilePath script = s.createScriptFile(root);
+            listener.getLogger().println(getEnvironment(node, additionalEnvironment).toString());
+            int r = launcher.launch().cmds(s.buildCommandLine(script)).envs(getEnvironment(node, additionalEnvironment)).stdout(listener).pwd(root).join();
 
+            if (r != 0) {
+                listener.getLogger().println("script failed!");
+                throw new AbortException("script failed!");
+            }
+
+            listener.getLogger().println("script executed successfully. private void executeScript");
+        }
+    }
+
+    /**
+     * @param computerList    the list of computers to upload the setup files and execute command line
+     * @param setupConfigItem the SetupConfigItem object
+     */
+    public void deployToComputers(List<Computer> computerList, SetupConfigItem setupConfigItem) {
+        for (Computer computer : computerList) {
+            try {
+                FilePath root = computer.getNode().getRootPath();
+                LogTaskListener listener = new LogTaskListener(LOGGER, Level.ALL);
+                deployToComputer(computer, root, listener, setupConfigItem);
+            } catch (IOException e) {
+                LOGGER.severe(e.getMessage());
+            } catch (InterruptedException e) {
+                LOGGER.severe(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * @param computerList the list of computers to upload the setup files and execute command line
+     * @param config       the SetupConfig object
+     */
+    public void deployToComputers(List<Computer> computerList, SetupConfig config) {
+        for (Computer computer : computerList) {
+            try {
+                FilePath root = computer.getNode().getRootPath();
+                LogTaskListener listener = new LogTaskListener(LOGGER, Level.ALL);
+                deployToComputer(computer, root, listener, config);
+            } catch (IOException e) {
+                LOGGER.severe(e.getMessage());
+            } catch (InterruptedException e) {
+                LOGGER.severe(e.getMessage());
+            }
+        }
+    }
+
+    public void executePrepareScripts(Computer c, SetupConfig config, TaskListener listener) {
+        for (SetupConfigItem setupConfigItem : config.getSetupConfigItems()) {
+            if (StringUtils.isBlank(setupConfigItem.getPrepareScript())) {
+                setupConfigItem.setPrepareScriptExecuted(true);
+            } else if (checkLabelsForComputerOrNull(c, setupConfigItem)) {
+                boolean successful = executeScriptOnMaster(
+                        setupConfigItem.getPrepareScript(),
+                        c,
+                        listener);
+                setupConfigItem.setPrepareScriptExecuted(successful);
+
+            }
+        }
+    }
+
+    public void executePreLaunchScripts(Computer c, SetupConfig config, TaskListener listener) throws AbortException {
+        for (SetupConfigItem setupConfigItem : config.getSetupConfigItems()) {
+            if (!StringUtils.isBlank(setupConfigItem.getPreLaunchScript()) && checkLabels(c, setupConfigItem)) {
+                boolean successful = executeScriptOnMaster(
+                        setupConfigItem.getPreLaunchScript(),
+                        c,
+                        listener);
+                if (!successful) {
+                    throw new AbortException("pre-launch script not executed successfully");
+                }
+            }
+        }
+    }
+
+    private boolean executeScriptOnMaster(String script, Computer c, TaskListener listener) {
+        // execute scripts on master relative to jenkins install dir
+        Node node = Jenkins.getInstance();
+        FilePath filePath = node.getRootPath();
+
+        boolean scriptExecuted;
         try {
-            execute(stopScript, listener, computer);
-        }  catch (Exception e) {
-            listener.getLogger().println("Failed executing script '" + stopScript + "'.");
-            e.printStackTrace(listener.getLogger());
+            executeScript(node, filePath, listener, script, createEnvVarsForComputer(c));
+            scriptExecuted = true;
+        } catch (Exception e) {
+            listener.getLogger().println("script failed with exception: " + e.getMessage());
+            scriptExecuted = false;
         }
+
+        return scriptExecuted;
     }
 
-    @Extension
-    public static class DescriptorImpl extends Descriptor<ComputerLauncher> {
-        public String getDisplayName() {
-            return "Start and stop this node on-demand";
+    private EnvVars createEnvVarsForComputer(Computer c) {
+        EnvVars additionalEnvironment = new EnvVars();
+        if (c != null) {
+            additionalEnvironment.put("NODE_TO_SETUP_NAME", c.getName());
+            Node node = c.getNode();
+            if (node != null) {
+                additionalEnvironment.put("NODE_TO_SETUP_LABELS", Util.join(node.getAssignedLabels(), " "));
+            }
         }
+        return additionalEnvironment;
     }
+
+    /**
+     * Returns the environment variables for the given node.
+     *
+     * @param node node to get the environment variables from
+     * @param additionalEnvironment environment added to the environment from the node. Take precedence over environment from the node.
+     * @return the environment variables for the given node
+     */
+    private EnvVars getEnvironment(Node node, EnvVars additionalEnvironment) {
+        EnvVars envVars = new EnvVars();
+        EnvironmentVariablesNodeProperty env = node.getNodeProperties().get(EnvironmentVariablesNodeProperty.class);
+        if (env != null) {
+            envVars.putAll(env.getEnvVars());
+        }
+        envVars.putAll(additionalEnvironment);
+        return envVars;
+    }
+
 
 }
-
